b81c9bdc72ab366f334cb8b00697eef1c5429701
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-26 13:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function curry(callback) {
-  
-}
+const sum = (...numbers) => numbers.reduce((total, number) => total + number, 0);
+const curriedSum = curry(sum);
 
-module.export=curry
+console.log(curriedSum()); // 0
+console.log(curriedSum(1)()); // 1
+console.log(curriedSum(1)(2)()); // 3
+console.log(curriedSum(1, 2)(3)(4, 5, 6)()); // 21
+console.log(curriedSum(1)); // [Function]
+console.log(curriedSum(1, 2)(3)); // [Function]
